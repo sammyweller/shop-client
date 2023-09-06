6b010709f5a8d9b91b49852a57c7c45749bf7692
Sammy Weller <sammymermell@gmail.com>
2023-09-06 12:32:32 -0400

    Adding details to game-card from API

--- src/components/game-card/game-card.jsx
+++ src/components/game-card/game-card.jsx
@@ -1,3 +1,6 @@
+import PropTypes from "prop-types";
+
+
 export const GameCard = ( { game, onGameClick } ) => {
     return (
         <div
@@ -6,6 +9,16 @@ export const GameCard = ( { game, onGameClick } ) => {
           }}
         >
           {game.title}
+          {game.image}
         </div>
       );
+  };
+
+
+  GameCard.propTypes = {
+    game: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired
+    }).isRequired,
+    onGameClick: PropTypes.func.isRequired
   };
